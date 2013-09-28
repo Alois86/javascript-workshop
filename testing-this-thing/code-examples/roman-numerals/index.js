e8ae5cc8733906c2c6f4e1da9fe839310468ff91
Philipp Fehre <philipp.fehre@googlemail.com>
2013-09-28 18:54:35 +0200

    basic cases before refactor

--- testing-this-thing/code-examples/roman-numerals/index.js
+++ testing-this-thing/code-examples/roman-numerals/index.js
@@ -1,5 +1,22 @@
 var convert = function(n) {
-  return "I"
+  var res = []
+  while(n >= 10) {
+   res.push("X")
+   n = n - 10
+  }
+  while(n >= 5) {
+   res.push("V")
+   n = n - 5
+  }
+  while(n >= 4) {
+   res.push("IV")
+   n = n - 4
+  }
+  while(n >= 1) {
+   res.push("I")
+   n = n - 1
+  }
+  return res.join("")
 }
 
 module.exports = convert
--- testing-this-thing/code-examples/roman-numerals/spec/roman.spec.js
+++ testing-this-thing/code-examples/roman-numerals/spec/roman.spec.js
@@ -1,7 +1,27 @@
 var convert = require("../index")
 
 describe("to_roman", function() {
-  it("outputs I for 1", function(){
+  it("outputs I for 1", function() {
     expect(convert(1)).toBe("I")
+  })
+
+  it("outputs II for 2", function() {
+    expect(convert(2)).toBe("II")
+  })
+
+  it("outputs IV for 5", function() {
+    expect(convert(4)).toBe("IV")
+  })
+
+  it("outputs V for 5", function() {
+    expect(convert(5)).toBe("V")
+  })
+
+  it("outputs X for 10", function() {
+    expect(convert(10)).toBe("X")
+  })
+
+  it("outputs XX for 20", function() {
+    expect(convert(20)).toBe("XX")
   })
 })
