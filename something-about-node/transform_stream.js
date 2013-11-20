3291dadbf6497f73eca09d1a46a56b0c1534371f
Julian Hollmann <julian.hollmann@googlemail.com>
2013-11-20 13:05:35 +0100

    Example based on question

--- something-about-node/transform_stream.js
+++ something-about-node/transform_stream.js
@@ -2,20 +2,23 @@ var Transform = require("stream").Transform
 var util = require("util")
 
 function ITransformThings(options) {
-  Transform.call(this, options)
-  this._writableState.objectMode = true
-  this._readableState.objectMode = false
+    Transform.call(this, options)
+    this._writableState.objectMode = true
+    this._readableState.objectMode = false
 }
 util.inherits(ITransformThings, Transform)
 
 ITransformThings.prototype._transform = function(chunk, enc, done) {
-  this.push(chunk.toString())
-  done()
+    if(typeof chunk === 'function') {
+        chunk = chunk();
+    }
+    this.push(chunk.toString());
+    done()
 }
 
 var ts = new ITransformThings()
 ts.pipe(process.stdout)
-ts.write(function(foo) { return foo })
+ts.write(function() { return 'awesome' })
 ts.write("bar")
 ts.write([1,2,3])
 ts.end()
