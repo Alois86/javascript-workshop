3f227eb7c909485dc748be9327f29a29296e8fe8
Julian Hollmann <julian.hollmann@googlemail.com>
2013-11-20 13:07:34 +0100

    Code Style..

--- something-about-node/transform_stream.js
+++ something-about-node/transform_stream.js
@@ -2,23 +2,25 @@ var Transform = require("stream").Transform
 var util = require("util")
 
 function ITransformThings(options) {
-    Transform.call(this, options)
-    this._writableState.objectMode = true
-    this._readableState.objectMode = false
+  Transform.call(this, options)
+  this._writableState.objectMode = true
+  this._readableState.objectMode = false
 }
 util.inherits(ITransformThings, Transform)
 
-ITransformThings.prototype._transform = function(chunk, enc, done) {
-    if(typeof chunk === 'function') {
-        chunk = chunk();
-    }
-    this.push(chunk.toString());
-    done()
+ITransformThings.prototype._transform = function (chunk, enc, done) {
+  if (typeof chunk === 'function') {
+    chunk = chunk();
+  }
+  this.push(chunk.toString());
+  done()
 }
 
 var ts = new ITransformThings()
 ts.pipe(process.stdout)
-ts.write(function() { return 'awesome' })
+ts.write(function () {
+  return 'awesome'
+})
 ts.write("bar")
-ts.write([1,2,3])
+ts.write([1, 2, 3])
 ts.end()
